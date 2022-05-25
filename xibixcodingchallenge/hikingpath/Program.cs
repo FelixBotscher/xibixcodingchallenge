713cefd7d5a9ae172f3c41b06604f000900f6753
botscher <45119276+FelixBotscher@users.noreply.github.com>
2022-05-25 14:00:41 +0200

    Fixed limited neighbours in the loop by changing the new starting Element from the old one

--- xibixcodingchallenge/hikingpath/Program.cs
+++ xibixcodingchallenge/hikingpath/Program.cs
@@ -15,54 +15,17 @@ namespace hikingpath
     {
         public static void Main(string[] args)
         {
-            //creating Lists
-            //List<Node> nodes = new List<Node>();
-            //List<Element> elements = new List<Element>();
-            //List<Value> values = new List<Value>();
-            List<Value> resultValues = new List<Value>(); //copy path into this file
+            List<Value> resultValues = new List<Value>(); //add peaks of the path into this file
             //check if arguments are correctly given
             //Map m = JsonConvert.DeserializeObject<Map>(File.ReadAllText(args[0]));
             //int n = int.Parse(args[1]);
-            string filePathofMesh =
-                "/Users/felixbotscher/Documents/Xibix/xibixcodingchallenge/xibixcodingchallenge/mesh_x_sin_cos_10000[82][1].json"; // won't be needed later
+            string filePathofMesh = //won't be needed later
+                "/Users/felixbotscher/Documents/Xibix/xibixcodingchallenge/xibixcodingchallenge/mesh_x_sin_cos_20000[1].json"; 
+                //"/Users/felixbotscher/Documents/Xibix/xibixcodingchallenge/xibixcodingchallenge/mesh_x_sin_cos_10000[82][1].json";
                 //"/Users/felixbotscher/Documents/Xibix/xibixcodingchallenge/xibixcodingchallenge/mesh[1].json"; 
-            int n = 50;
+            int n = 20;
             //if(n < 1 || n > m.elements.Count) printResult(null);
-            /*-----------------------Tests----------------------------
-            //creating Objects and add them to the List
-            Node node1 = new Node(0, 0, 0);
-            Node node2 = new Node(1, 0, 1);
-            Node node3 = new Node(2, 0, 2);
-            int nodeNr1 = 0, nodeNr2 = 1, nodeNr3 = 2;
-            List<int> intListe = new List<int>();
-            intListe.Add(nodeNr1);
-            intListe.Add(nodeNr2);
-            intListe.Add(nodeNr3);
-            nodes.Add(node1);
-            nodes.Add(node2);
-            nodes.Add(node3);
-            Element element1 = new Element(0, intListe);
-            elements.Add(element1);
-            Value value1 = new Value(0, 2.4);
-            values.Add(value1);
-            Console.WriteLine(node1);
-            Console.WriteLine(element1);
-            Console.WriteLine(value1);
-            // works great as expected 
-            //adding Lists to the Map Object
-            Map map = new Map(nodes, elements, values);
-            string filePath =
-                "/Users/felixbotscher/Documents/Xibix/xibixcodingchallenge/xibixcodingchallenge/test.json";
-
-            //test serialization of List Objects first
-            string convert = JsonConvert.SerializeObject(nodes, Formatting.Indented);
-            Console.WriteLine(convert);
-            convert = JsonConvert.SerializeObject(element1, Formatting.Indented);
-            Console.WriteLine(convert);
-            convert = JsonConvert.SerializeObject(value1, Formatting.Indented);
-            Console.WriteLine(convert);
-            convert = JsonConvert.SerializeObject(map, Formatting.Indented);
-            Console.WriteLine(convert);
+            /* ---------Code for serialization-------------------------------------
             //string convert = JsonConvert.SerializeObject(map/values, Formatting.Indented);
             using (StreamWriter file = File.CreateText(filePath))
             {
@@ -77,27 +40,25 @@ namespace hikingpath
             */
             //----------------------Program--------------------------------------
             Map meshMap = JsonConvert.DeserializeObject<Map>(File.ReadAllText(filePathofMesh));
-            Value highestPoint = findHighestPoint(meshMap.values);
+            
+            Value highestPoint = FindHighestPoint(meshMap.values);
             //Console.WriteLine("Der höchste Punkt auf der Karte ist auf dem Element mit der ID {0} und dem Wert {1}.", highestPoint.element_id, highestPoint.value);
             resultValues.Add(highestPoint);
             //printResult(resultValues);
             Element elementStart = meshMap.elements.Find(x => x.id == highestPoint.element_id);
+            Value oldValue = highestPoint;
             List<Element> neighbours = new List<Element>();
             List<Value> valuesOfNeighbours = new List<Value>();
             List<Value> sortedNeighbours = new List<Value>();
-            for (int y = 0; y < n; y++)
+            for (var y = 2; y<=n ; y++)
             {
+                //set new elementStart from the Value before
+                oldValue = resultValues.Last();
+                elementStart = meshMap.elements.Find(x => x.id == oldValue.element_id);
                 //find the neighbours
-                neighbours = findNeighbours(meshMap.nodes, elementStart, meshMap.elements);
+                neighbours = FindNeighbours(meshMap.nodes, elementStart, meshMap.elements);
                 //change List<Elements> to List<Values> because we need to compare the value of Value
-                valuesOfNeighbours = returnValue(neighbours, meshMap.values); //alternative: meshMap.values.Find(x => x.element_id == i.id)
-                /*
-                Console.WriteLine("Values of neighbours:");
-                foreach (var i in valuesOfNeighbours)
-                {
-                    Console.WriteLine(i.ToString());
-                }
-                */
+                valuesOfNeighbours = ReturnValue(neighbours, meshMap.values); //alternative: meshMap.values.Find(x => x.element_id == i.id)
                 //loop proves if we have visited the element already
                 foreach (var ivalue in valuesOfNeighbours.ToList())
                 {
@@ -113,23 +74,15 @@ namespace hikingpath
                     break;
                 }
                 sortedNeighbours = valuesOfNeighbours.OrderByDescending(val=>val.value).ToList();
-                /*
-                Console.WriteLine("Sorted neighbours:");
-                foreach (var i in sortedNeighbours)
-                {
-                    Console.WriteLine(i.ToString());
-                }
-                */
                 resultValues.Add(sortedNeighbours.First());
-                //Console.WriteLine("\nOldElement:" + sortedNeighbours.First());
             }
-            printResult(resultValues);
+            PrintResult(resultValues);
         }
         
         /**
          * function that returns the highest point of the map by using th max and first function on lists
          */
-        public static Value findHighestPoint(List<Value> values)
+        private static Value FindHighestPoint(List<Value> values)
         {
             double greatesVal = values.Max(val => val.value);
             return values.First(x => x.value == greatesVal);
@@ -138,19 +91,19 @@ namespace hikingpath
         /**
          * function that finds the three neighbours of a element 
          */
-        public static List<Element> findNeighbours(List<Node> nodes, Element elementStart, List<Element> elements)
+        private static List<Element> FindNeighbours(List<Node> nodes, Element elementStart, List<Element> elements)
         {
             var nodeId1 = elementStart.nodes[0]; // first Value of List in Element of elementStart
             var nodeId2 = elementStart.nodes[1]; // second Value of List in Element of elementStart
             var nodeId3 = elementStart.nodes[2]; // third Value of List in Element of elementStart
             List<Node> elementNodesList =
                 nodes.FindAll(lambda => lambda.id == nodeId1 || lambda.id == nodeId2 || lambda.id == nodeId3); //retrieve a List of Nodes of the elementStart
-            List<Element> oneElements = containsID(nodeId1, elements);
-            List<Element> oneElementsIntersectionTwo = containsID(nodeId2, oneElements);
-            List<Element> twoElements = containsID(nodeId2, elements);
-            List<Element> twoElementsIntersectionThree = containsID(nodeId3, twoElements);
-            List<Element> threeElements = containsID(nodeId3, elements);
-            List<Element> threeElementsIntersectionOne = containsID(nodeId1, threeElements);
+            List<Element> oneElements = ContainsID(nodeId1, elements);
+            List<Element> oneElementsIntersectionTwo = ContainsID(nodeId2, oneElements);
+            List<Element> twoElements = ContainsID(nodeId2, elements);
+            List<Element> twoElementsIntersectionThree = ContainsID(nodeId3, twoElements);
+            List<Element> threeElements = ContainsID(nodeId3, elements);
+            List<Element> threeElementsIntersectionOne = ContainsID(nodeId1, threeElements);
 
             List<Element> result = oneElementsIntersectionTwo.Concat(twoElementsIntersectionThree)
                 .Concat(threeElementsIntersectionOne).ToList();
@@ -162,14 +115,14 @@ namespace hikingpath
          * Betrachtet eine Liste von Elementen und prüft ob die gegebene nodeId gleich einer von den NodeIds des Elementes ist
          * Rückgabeparameter ist dann eine Liste an Elementen vom Typ Element
          */
-        public static List<Element> containsID(int nodeId, List<Element> elements)
+        private static List<Element> ContainsID(int nodeId, List<Element> elements)
         {
             return elements.FindAll(elem => elem.nodes[0] == nodeId || elem.nodes[1] == nodeId || elem.nodes[2] == nodeId);
         }
         /**
          * returnValue finds all value elements which have the same id as in the elements list
          */
-        public static List<Value> returnValue(List<Element> elements, List<Value> values)
+        private static List<Value> ReturnValue(List<Element> elements, List<Value> values)
         {
             List<Value> result = new List<Value>();
             foreach (var element in elements)
@@ -182,7 +135,7 @@ namespace hikingpath
         /**
          * returns the highest neighbour Value element of the given list
          */
-        public static void printResult(List<Value> values)
+        private static void PrintResult(List<Value> values)
         {
             Console.WriteLine("[");
             foreach (var value in values)
